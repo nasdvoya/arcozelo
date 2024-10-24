ffc0f51358ae202f25fb11504333dba7a6da40a5
sasha <nasdvoya@proton.me>
2024-10-24 22:44:23 +0100

    Required phone personal phone number rules

--- src/client/scripts/page_loader.js
+++ src/client/scripts/page_loader.js
@@ -1,11 +1,10 @@
 function page_loader() {
     return {
-        currentSlide: 3,
+        currentSlide: 2,
         loadedContent: '', // Holds the currently loaded slide content
         slides: [
             './pages/home.html',
             './pages/events.html', // Path to the first slide
-            './pages/permanent-profile.html', // Path to the second slide
             './pages/temporary-profile.html'  // Path to the third slide
         ],
         // Preload the next slide
