60355972a79227f437d9e31b98ecc02adc78cb1d
sasha <nasdvoya@proton.me>
2025-01-11 20:05:22 +0000

    Configuration file

--- src/engine/src/main.rs
+++ src/engine/src/main.rs
@@ -1,9 +1,11 @@
 use std::net::TcpListener;
-
+use arcozelo_engine::configuration::get_configuration;
 use arcozelo_engine::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let configuration = get_configuration().expect("Failed to get configuration");
+
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     println!("http:://127.0.0.1:{}", port);
