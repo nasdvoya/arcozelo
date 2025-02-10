41a8fc14dcac287be98163878c0c26e69de34234
sasha <nasdvoya@proton.me>
2025-02-10 00:27:38 +0000

    Redirect is ducked

--- src/engine/app/Endpoints/Donors.cs
+++ src/engine/app/Endpoints/Donors.cs
@@ -9,12 +9,15 @@ public static class Donors
     public static void AddDonorEndpoints(this IEndpointRouteBuilder app)
     {
         app.MapPost(
-                "/donor-profile/",
-                async ([FromBody] DonorProfile profile, DonorDbContext db) =>
+                "/donor/profile/",
+                async ([FromBody] DonorProfile profile, DonorDbContext db, HttpResponse response) =>
                 {
+                    Console.WriteLine(profile.Email);
                     db.DonorProfiles.Add(profile);
                     await db.SaveChangesAsync();
-                    return Results.Created($"/donor-profile/{profile.Id}", profile);
+                    response.Headers["HX-Redirect"] = "/index";
+                    return Results.Created($"/donor/profile/{profile.Id}", profile);
+                    // return Results.Json(new { success = true });
                 }
             )
             .WithName("test")
@@ -23,11 +26,20 @@ public static class Donors
             .WithDescription("description");
 
         app.MapGet(
-            "/donor-profile/{id}",
+            "/donor/profile/{id}",
             async (Guid id, DonorDbContext db) =>
             {
                 var donor = await db.DonorProfiles.FindAsync(id);
                 return donor is not null ? Results.Ok(donor) : Results.NotFound();
+            }
+        );
+
+        app.MapPost(
+            "/donor/test",
+            async (HttpContext context) =>
+            {
+                context.Response.Headers["HX-Redirect"] = "/";
+                return Results.Ok();
             }
         );
     }
